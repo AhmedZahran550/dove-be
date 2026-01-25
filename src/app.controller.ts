7831beb928059f048edafad3a37bde0d917ae7b5
AhmedZahran550 <ahmdzhran50@gmail.com>
2026-01-26 00:40:31 +0200

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './modules/auth/decorators/public.decorator';
 
 @Controller()
+@Public()
 export class AppController {
   constructor() {}
 
